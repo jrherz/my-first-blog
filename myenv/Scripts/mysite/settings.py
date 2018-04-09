4dc48496be79de91a3943a1a88cc72e7d0b222ab
Jordan H <jordan@jrherz.com>
2018-04-09 17:14:47 -0400

    Hopefully solved the problem with CSS

--- myenv/Scripts/mysite/settings.py
+++ myenv/Scripts/mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '$^k3np5e1zz(vt+r#$bz^0wo&#+nwt4(683^vabmz7zjv)!2-$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'pythonanywhere.com','jrherz.pythonanywhere.com']
 
@@ -120,3 +120,4 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+print (STATIC_ROOT)
